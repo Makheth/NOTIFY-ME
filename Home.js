836772fd6c10fcb90556e48d0f1e512eee2cb707
Makheth <makhethamamoabi2@gmail.com>
2024-05-16 20:17:38 +0530

    group assignment

--- Home.js
+++ Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Text, SafeAreaView, Button, View, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faTag, faMapMarkerAlt, faThermometer } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Home() {
   const navigation = useNavigation()
